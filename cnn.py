43878ba8ce1b81ade122e294a9a3c75cb0071d62
Gerald <geraldwainaina25@gmail.com>
2023-02-08 15:14:56 +0300

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -22,4 +22,157 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+
+#output number of datasets in Train and Test
+print("Number of training examples: {}".format(num_test_examples))
+print("Number of test examples: {}".format(num_test_examples))
+
+#Preprocess the data
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+#The map function applies normalize func to train and test datasets
+train_dataset = train_dataset.map(normalize)
+test_dataset = train_dataset.map(normalize)
+
+#Store images in cache to keep them in memory to make training faster
+train_dataset = train_dataset.cache()
+test_dataset = test_dataset.cache()
+
+#Start --> means this code upto stop can be omitted without affecting your program
+#Feel free to delete it
+#We will be exploring our data
+
+for image, label in test_dataset.take(1):
+    break
+image = image.numpy().reshape((28, 28))
+
+plt.figure()
+plt.imshow(image, camp=plt.cm.binary)
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+plt.figure(figsize=(10, 10))
+i = 0
+for (image, label) in test_dataset.take(25):
+    image = image.numpy().reshape((28, 28))
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.xlabel(class_names[label])
+    i += 1
+plt.show()
+
+#Stop --> End of deletable code
+
+#building our model and setting up
+#Convolutional neural network
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation=tf.nn.relu,
+                           input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+    tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation=tf.nn.relu),
+    tf.keras.MaxPooling2D((2, 2), strides=2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+#compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+#Train the model
+BATCH_SIZE = 32
+train_dataset = train_dataset.cache().repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.cache().batch(BATCH_SIZE)
+
+model.fit(train_dataset, epochs=10, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+
+#Accuracy Evaluation
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
+print('Accuracy on test dataset:', test_accuracy)
+
+#Making predictions
+for test_images, test_labels in test_dataset.take(1):
+    test_images = test_images.numpy()
+    test_labels = test_labels.numpy()
+    predictions = model.predict(test_images)
+predictions.shape
+predictions[0]
+np.argmax(predictions[0])
+test_labels[0]
+
+def plot_image(i, predictions_array, true_labels, images):
+    predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img[..., 0], cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+                                         color=color)
+
+    def plot_value_array(i, predictions_array, true_label):
+        predictions_array, true_label = predictions_array[i], true_label[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        thisplot = plt.bar(range(10), predictions_array, color="#777777")
+        plt.ylim([0, 1])
+        predicted_label = np.argmax(predictions_array)
+
+        thisplot[predicted_label].set_color('red')
+        thisplot[true_label].set_color('blue')
+
+        i = 0
+        plt.figure(figsize=(6, 3))
+        plt.subplot(1, 2, 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(1, 2, 2)
+        plot_value_array(i, predictions, test_labels)
+
+
+        num_rows = 5
+        num_cols = 3
+        num_images = num_rows*num_cols
+        plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+        for i in range(num_images):
+            plt.subplot(num_rows, 2*num_cols, 2*i+1)
+            plot_image(i, predictions, test_labels, test_images)
+            plt.subplot(num_rows, 2*num_cols, 2*i+2)
+            plot_value_array(i, predictions, test_labels)
+
+        #Grab an image from the test dataset
+        img = test_images[0]
+        print(img.shape)
+
+        img = np.array([img])
+        print(img.shape)
+
+        #predict the image
+        predictions_single = model.predict(img)
+        print(predictions_single)
+
+        plot_value_array(0, predictions_single, test_labels)
+        _ = plt.xticks(range(10), class_names, rotation=45)
+
+        np.argmax(predictions_single[0])
+
 
